5c132658d7945b05d7e45e2d318f7b0dba616df1
Michael Harhay <Michael_Harhay@arxtron.com>
2025-11-06 10:33:18 -0500

    Updated plcAutoTest.py for non-GUI version

--- plcAutoTest.py
+++ plcAutoTest.py
@@ -1,119 +1,105 @@
-import sys
 from pylogix import PLC
 from time import sleep
-import time
 
-plc = PLC()
 
-def nest1_cylinder_move(self):
+# CTS functions
+def nest1_cylinder_move():
   current = plc.Read('Program:MainProgram.Nest1Cylinder.PB')
-  self.plc.Write('Program:MainProgram.Nest1Cylinder.PB', not current.Value)
+  plc.Write('Program:MainProgram.Nest1Cylinder.PB', not current.Value)
 
-def nest2_cylinder_move(self):
+def nest2_cylinder_move():
   current = plc.Read('Program:MainProgram.Nest2Cylinder.PB')
-  self.plc.Write('Program:MainProgram.Nest2Cylinder.PB', not current.Value)
-  
-def plc_connect(self):
-  self.plc.IPAddress = self.plc_ip_address_edit.text()
-  self.cts_controls_group.setEnabled(True)
-  self.robot_controls_group.setEnabled(True)
-  
-def move_to_position(self, position, timeout=10.0):
-  self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', position, datatype=199)
-  
-  start_time = time.time()
-  while time.time() - start_time < timeout:
-    inPos = self.plc.Read('Program:MainProgram.RobotOutputs.ob_InPos', datatype=193)
-    if inPos.Value == 1:
-        return True
-    sleep(0.1)  # non-blocking
+  plc.Write('Program:MainProgram.Nest2Cylinder.PB', not current.Value)
 
-  return False # returns false if timeout occurs
-  
-def robot_ungrip(self):
-  self.plc.Write('Program:MainProgram.RobotGripper.PB', False)
-  sleep(0.2)
-
-def robot_grip(self):
-  self.plc.Write('Program:MainProgram.RobotGripper.PB', True)
-  sleep(0.2)
-
-def robot_move(self, target):
-  self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', target, dataype=199)
-
-def update_status(self, status):
-  self.status_label.setText(status)
-
-def simulate_cts_test(self):
+def simulate_cts_test():
   sleep(1.0)
-  self.nest1_cylinder_clicked()
+  nest1_cylinder_move()
   sleep(3.0)
-  self.nest1_cylinder_clicked()
+  nest1_cylinder_move()
   sleep(1.0)
 
+
+# Robot functions
+def robot_ungrip():
+  plc.Write('Program:MainProgram.RobotGripper.PB', False)
+  sleep(0.2)
+
+def robot_grip():
+  plc.Write('Program:MainProgram.RobotGripper.PB', True)
+  sleep(0.2)
+
+def robot_move(target):
+  plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', target, datatype=199)
+  
+  while True:
+    inPos = plc.Read('Program:MainProgram.RobotOutputs.ob_InPos', datatype=193)
+    if inPos.Value == 1:
+      return True
+    sleep(0.1)
+
+
+# Main test function
 def start_test(self):
 
   # Init
-  print("Status: Initializing")
-  self.plc.Write('Program:MainProgram.RobotInputs.ib_Reset', 1, datatype=193)
+  print("\nStatus: Initializing")
+  plc.Write('Program:MainProgram.RobotInputs.ib_Reset', 1, datatype=193)
   sleep(1.0)
-  self.plc.Write('Program:MainProgram.RobotInputs.ib_Reset', 0, datatype=193)
-  self.robot_ungrip()
-  self.move_to_position(0)
+  plc.Write('Program:MainProgram.RobotInputs.ib_Reset', 0, datatype=193)
+  robot_ungrip()
+  robot_move(0)
   
-  # Barcode -> fail
-  self.update_status("BARCODE -> FAIL") 
-  self.plc.Write('Program:MainProgram.RobotInputs.ib_Start', 1, datatype=193)
-  self.robot_grip()
-  self.move_to_position(1)  # BARCODE
+  # BARCODE -> FAIL
+  print("\nSequence: BARCODE -> FAIL") 
+  plc.Write('Program:MainProgram.RobotInputs.ib_Start', 1, datatype=193)
+  robot_grip()
+  robot_move(1)  # BARCODE
 
-  self.move_to_position(5)  # FAIL
-  self.robot_ungrip()
-  self.move_to_position(0)  # LOADING
+  robot_move(5)  # FAIL
+  robot_ungrip()
+  robot_move(0)  # LOADING
 
+  # NEST1 -> FAIL
+  print("\nSequence: NEST1 -> FAIL") 
+  robot_move(0)
+  robot_grip()
+  robot_move(1)  # BARCODE
 
-  # Nest 1 -> fail
-  # self.update_status("NEST1 -> FAIL") 
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 0, datatype=199) # LOADING
-  # self.plc.Write('Program:MainProgram.RobotInputs.ib_Start', 1, datatype=193)
-  # self.robot_grip()
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 1, datatype=199) # BARCODE
-  # sleep(2.0)
+  robot_move(2)  # NEST1
+  robot_ungrip()
+  robot_move(1)  # BARCODE
+  simulate_cts_test()
+  robot_move(2)  # NEST1
+  robot_grip()
 
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 2, datatype=199) # NEST1
-  # self.robot_ungrip()
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 1, datatype=199) # BARCODE
-  # simulate_cts_test()
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 2, datatype=199) # NEST1
-  # self.robot_grip()
+  robot_move(5)  # FAIL
+  robot_ungrip()
+  robot_move(0)  # LOADING
 
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 5, datatype=199) # FAIL
-  # self.robot_ungrip()
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 0, datatype=199) # LOADING
+  # NEST1 -> PASS
+  print("\nSequence: NEST1 -> PASS") 
+  robot_move(0)
+  robot_grip()
+  robot_move(1)  # BARCODE
 
+  robot_move(2)  # NEST1
+  robot_ungrip()
+  robot_move(1)  # BARCODE
+  simulate_cts_test()
+  robot_move(2)  # NEST1
+  robot_grip()
 
-  # # Nest 1 -> pass
-  # self.update_status("NEST1 -> PASS") 
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 0, datatype=199) # LOADING
-  # self.plc.Write('Program:MainProgram.RobotInputs.ib_Start', 1, datatype=193)
-  # self.robot_grip()
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 1, datatype=199) # BARCODE
-  # sleep(2.0)
-
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 2, datatype=199) # NEST1
-  # self.robot_ungrip()
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 1, datatype=199) # BARCODE
-  # simulate_cts_test()
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 2, datatype=199) # NEST1
-  # self.robot_grip()
-
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 4, datatype=199) # PASS
-  # self.robot_ungrip()
-  # self.plc.Write('Program:MainProgram.RobotInputs.iW_TargetPosition', 0, datatype=199) # LOADING
+  robot_move(4)  # PASS
+  robot_ungrip()
+  robot_move(0)  # LOADING
 
 
   
 if __name__ == '__main__':
+  print("\nInitializing PLC...")
   plc = PLC()
+  plc.IPAddress = '192.168.250.1'
+
+  print("\nRunning test sequences...")
   start_test()
         
